24cb84c5ac6fddaf0bcef6f82454415fe26f698d
kokarare1212 <kokarare1212@gmail.com>
2020-06-05 15:03:26 +0900

    Commit

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = ["requests"]
 
 setup(
     name="Radiko",
-    version="1.1.2",
+    version="1.1.3",
     description="Python製の非公式Radikoライブラリ",
     url="https://github.com/kokarare1212/Radiko/",
     author="kokarare1212",
