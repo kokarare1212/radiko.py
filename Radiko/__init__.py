26f75325fd60f2947d257501f02de77360230c79
こうから <kokarare1212@gmail.com>
2020-10-04 15:24:55 +0900

    Update __init__.py

--- Radiko/__init__.py
+++ Radiko/__init__.py
@@ -1,4 +1,10 @@
-
-"""Radiko
+##############################################################
+### Radiko                                                 ###
+### Copyright © 2020 kokarare1212 All rights reserved.     ###
+###                                                        ###
+### This software is released under the Apache License 2.0 ###
+### see http://www.apache.org/licenses/LICENSE-2.0         ###
+##############################################################
+""" Radiko
 Python製の非公式Radikoライブラリ
 """
