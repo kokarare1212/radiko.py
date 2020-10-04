0496c868c4e02c2d20dd1080b498c04b3d92b36b
こうから <kokarare1212@gmail.com>
2020-10-04 16:38:04 +0900

    Update __init__.py

--- Radiko/__init__.py
+++ Radiko/__init__.py
@@ -5,7 +5,9 @@
 ### This software is released under the Apache License 2.0 ###
 ### see http://www.apache.org/licenses/LICENSE-2.0         ###
 ##############################################################
-"""Form a complex number.
+"""
+
+  Form a complex number.
 
   Keyword arguments:
   real -- the real part (default 0.0)
