cbd5ea09b8d4db052fdc98e6789fe10e9ea33d4e
こうから <kokarare1212@gmail.com>
2020-10-04 16:36:07 +0900

    Update __init__.py

--- Radiko/__init__.py
+++ Radiko/__init__.py
@@ -5,7 +5,12 @@
 ### This software is released under the Apache License 2.0 ###
 ### see http://www.apache.org/licenses/LICENSE-2.0         ###
 ##############################################################
+"""Form a complex number.
 
+  Keyword arguments:
+  real -- the real part (default 0.0)
+  imag -- the imaginary part (default 0.0)
+"""
 from .Radiko import Radiko, RadikoException
 
 __all__ = ["Radiko", "RadikoException"]
