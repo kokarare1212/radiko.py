0d2cb75458e6c3e2ef656dea93d179e9f6d422bc
kokarare1212 <kokarare1212@gmail.com>
2021-01-10 16:59:23 +0900

    commit

--- Radiko/__init__.py
+++ Radiko/__init__.py
@@ -1,11 +1,11 @@
 ##############################################################
-### Radiko                                                 ###
+### radiko                                                 ###
 ### Copyright © 2020 kokarare1212 All rights reserved.     ###
 ###                                                        ###
 ### This software is released under the Apache License 2.0 ###
 ### see http://www.apache.org/licenses/LICENSE-2.0         ###
 ##############################################################
 
-from .Radiko import *
+from .radiko import RadikoClient, __version__
 
-__all__ = ["Radiko", "RadikoException"]
+__all__ = ["RadikoClient", "__version__"]
